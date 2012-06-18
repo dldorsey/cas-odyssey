e4a61dc7250c80212cf64b783da78cb9eeac1578
Danny Dorsey <dldorsey@gmail.com>
2012-06-17 23:15:57 -0400

    Added *_ExternalURL for services to the scenario data. Updated CAS list logic to test for the service so that only a CAS that provides the service will appear on the list for selection.

--- CAS_rules.ps1
+++ CAS_rules.ps1
@@ -9,33 +9,43 @@ foreach ($server in $serverList)
   if ($server.IsMBX -eq $True) {$mbxList += $server}
 }
 
-#TODO List all Mailbox servers by number
+#List all Mailbox servers by number
 $mbxCount = 1
 foreach ($mbxServer in $mbxList)
 {
   write-host $mbxCount, $mbxServer.Name, $mbxServer.Version, $mbxServer.ServicePack, $mbxServer.Site
   $mbxCount += 1
 }
-$userMailboxServer = Read-Host "Please choose a Mailbox Server by number from the list above."
-#TODO List all services by number
-$serviceList = @("Outlook Web App", "Exchange ActiveSync", "Exchange Control Panel", "Exchange Web Services", "POP3", "IMAP4")
+$accessedMBX = Read-Host "Please choose a Mailbox Server by number from the list above."
+#Convert from integer selection to object and adjust by 1 for 0 based array index
+$accessedMBX = $mbxList[$accessedMBX - 1]
+
+#List all services by number
+$serviceList = @(@("Outlook Web App","OWA_ExternalURL"), @("Exchange ActiveSync", "EAS_ExternalURL"), @("Exchange Control Panel", "ECP_ExternalURL"), @("Exchange Web Services", "EWS_ExternalURL"), @("POP3","POP_InternalConnectionSettings"), @("IMAP4","IMAP_InternalConnectionSettings"))
 $serviceCount = 1
 foreach ($service in $serviceList)
 {
-  write-host $serviceCount, $service
+  write-host $serviceCount, $service[0]
   $serviceCount += 1
 }
 $accessedService = Read-Host "Please choose a Service by number from the list above."
-#TODO List all CAS with ExternalURL for the selected service by number
+#Convert from integer selection to object and adjust by 1 for 0 based array index
+$accessedService = $serviceList[$accessedService - 1]
+
+#List all CAS with ExternalURL for the selected service by number
 $casCount = 1
 foreach ($casServer in $casList)
 {
-  if ($casServer.ExternalURL)
+  #For some reason, the propterty needs to be evaluated before it will work - thus the parenthesis around $accessedService[1]
+  if ($casServer.($accessedService[1]))
     {
       Write-Host $casCount, $casServer.Name, $casServer.Version, $casServer.ServicePack, $casServer.Site
     }
   $casCount += 1
 }
 $accessedCAS = Read-Host "Please choose a CAS by number from the list above."
+#Convert from integer selection to object and adjust by 1 for 0 based array index
+$accessedCAS = $casList[$accessedCAS - 1]
 
-Write-Host "Simulating a user from mailbox server", $mbxList[($accessedMBX - 1)].Name, "accessing", $serviceList[($accessedService - 1)], "though client access server", $casList[($accessedCAS - 1)].Name
+Write-Host "Simulating a user from mailbox server", $accessedMBX.Name, "accessing", $accessedService[0], "though client access server", $accessedCAS.Name
+
